a729bc0fbe0cf537ac7e97d2b179809b9f4cd9ab
shivam2003-ai <shivam@newstok.ai>
2024-12-01 17:31:04 +0530

    connecting both ends

--- app/page.tsx
+++ app/page.tsx
@@ -7,14 +7,28 @@ import axios from "axios";
 export default function Home() {
   const [videoUrl, setVideoUrl] = useState<string>("");
   const [isSet, setIsSet] = useState<boolean>(false);
-  const uploadUrl = ()=>{
-    const data = axios.post('http://localhost:8000/upload', {video_url: videoUrl});
+  function extractVideoID(url: string, param: string):string | null{
+    try{
+
+      const urlObj = new URL(url);
+      return urlObj.searchParams.get(param) 
+    }catch(err){
+      console.error("Invalid URL:", err);
+      return null;
+    }
+  }
+  const uploadUrl = async()=>{
+    const truncatedUrl = await extractVideoID(videoUrl, "si")
+    const data = axios.post('http://localhost:8000/upload', {video_url: truncatedUrl});
     data.then((res)=>{
       if(res.status == 200){
         console.log("Uploaded Successfully");
+        
         setIsSet(true);
       }
+      console.log(res)
     })
+    console.log(truncatedUrl);
   }
   return (<div><InputFields addUrl={(url)=>{
     setVideoUrl(url);
