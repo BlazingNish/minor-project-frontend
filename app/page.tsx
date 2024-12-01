640514f331ada17f8f1c16ae04f160f9ff095247
Nishant <nishant.e.ekka@gmail.com>
2024-12-01 19:31:51 +0530

    Added redirect to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,38 +1,5 @@
-"use client"
-
-import InputFields from "@/components/inputFields";
-import {useState} from "react"; 
-import axios from "axios";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  const [videoUrl, setVideoUrl] = useState<string>("");
-  const [isSet, setIsSet] = useState<boolean>(false);
-  function extractVideoID(url: string, param: string):string | null{
-    try{
-
-      const urlObj = new URL(url);
-      return urlObj.searchParams.get(param) 
-    }catch(err){
-      console.error("Invalid URL:", err);
-      return null;
-    }
-  }
-  const uploadUrl = async()=>{
-    const truncatedUrl = await extractVideoID(videoUrl, "si")
-    const data = axios.post('http://localhost:8000/upload', {video_url: truncatedUrl});
-    data.then((res)=>{
-      if(res.status == 200){
-        console.log("Uploaded Successfully");
-        
-        setIsSet(true);
-      }
-      console.log(res)
-    })
-    console.log(truncatedUrl);
-  }
-  return (<div><InputFields addUrl={(url)=>{
-    setVideoUrl(url);
-    uploadUrl();
-  }}/></div>
-  );
+  redirect("/chat");
 }
